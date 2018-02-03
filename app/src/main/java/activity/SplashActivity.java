f20090adfab422973ea0b6cfd817dc675945d560
myjohnsonkilly <1245145380@qq.com>
2018-02-03 23:18:07 +0800

    ..

---
+++ app/src/main/java/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import family.live.R;
+
+/**
+ * Created by mac on 18/2/3.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainTabsActivity.class));
+            }
+        }, 2000);
+
+        setContentView(R.layout.act_splash);
+    }
+}
